aa4c4c8aba29b17121c8faeb1fe32dc1a65c439b
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-02-04 17:53:11 -0500

    finished css and started working on scriptjs

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+const locationButton = document.querySelector(".location-btn");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const weatherCardsDiv = document.querySelector(".weather-cards");
+const lastSearchedCityElement = document.getElementById("lastSearchedCity");
+const API_KEY = "6d2c5466092de2babc8fd56a62672dee";
