6a8e4f5ba354d504dc4cbaec6ad65eb27fe482a0
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-02-04 18:12:02 -0500

    wrote code for weather details

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,70 @@ const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 const lastSearchedCityElement = document.getElementById("lastSearchedCity");
 const API_KEY = "6d2c5466092de2babc8fd56a62672dee";
+
+
+const createWeatherCard = (cityName, weatherItem, index) => {
+    if (index === 0) {
+        return `<div class="details">
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                </div>
+                <div class="icon">
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>${weatherItem.weather[0].description}</h6>
+                </div>`;
+    } else {
+        return `<li class="card">
+                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                </li>`;
+    }
+}
+
+
+
+
+
+const getWeatherDetails = async (cityName, latitude, longitude) => {
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+    try {
+        const response = await fetch(WEATHER_API_URL);
+        const data = await response.json();
+
+        const uniqueForecastDays = [];
+        const fiveDaysForcast = data.list.filter(forcast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!uniqueForecastDays.includes(forecastDate)) {
+                return uniqueForecastDays.push(forecastDate);
+            }  
+        });
+   
+        cityInput.value = "";
+        currentWeatherDiv.innerHTML = "";
+        weatherCardsDiv.innerHTML = "";
+
+        fiveDaysForecast.forEach((weatherItem, index) => {
+            const html = createWeatherCard(cityName, weatherItem, index);
+            if (index === 0) {
+                currentWeatherDiv.insertAdjacentHTML("beforeend", html);
+            } else {
+                weatherCardsDiv.insertAdjacentHTML("beforeend", html);
+            }
+        });
+
+        // Save last searched city to local storage
+        saveLastSearchedCity(cityName);
+        // Display last searched city in the frontend
+        lastSearchedCityElement.textContent = getLastSearchedCity();
+    } catch (error) {
+        alert("An error occurred while fetching the weather forecast!");
+    }
+
+
+};
